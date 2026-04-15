2a3b489275085a80e57cc8ae69fda1c9d280c229
Karim Sayed <karimsayed218m@gmail.com>
2026-04-15 19:06:39 +0200

    final edit on common questions

--- features/landingPage/components/CommonQuestions/QuestionsList.tsx
+++ features/landingPage/components/CommonQuestions/QuestionsList.tsx
@@ -5,7 +5,7 @@ function QuestionsList() {
   return (
     <div>
       {questions.map((item, index) => (
-                <div key={index} className='flex gap-10 items-center border-b-2 p-5 border-gray-200 hover:translate-x-[-20px] transition-transform duration-200'>
+                <div key={item.id} className='flex gap-10 items-center border-b-2 p-5 border-gray-200 hover:translate-x-[-20px] transition-transform duration-200'>
                     <span className='text-xl font-bold px-2 text-blue-600 bg-blue-100'>+</span>
                     <p className='text-lg font-bold'>{item.question}</p>
                 </div>
